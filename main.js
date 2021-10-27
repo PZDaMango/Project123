397ab5890ff59aede63e52f0c98c22b2a1cd4e4d
PZWhitehatJr <82187898+PZWhitehatJr@users.noreply.github.com>
2021-10-26 20:28:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+}
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+function gotPoses(reults){
+    if(results.length > 0)
+    {
+        console.log(results);
+    }
+}
+function draw() {
+    background('#00FFFF');
+}
